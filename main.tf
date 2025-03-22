4a575af64a43b46621a3a720668d80adfa77bd30
Sivarajesh Balamurali <b.sivarajesh20@gmail.com>
2025-03-21 23:17:16 -0300

    bug: Fixing

--- main.tf
+++ main.tf
@@ -31,142 +31,52 @@ resource "google_artifact_registry_repository" "artifact_registry_repo" {
   project = "k8s-assignement"
 }
 
-# resource "google_cloudbuildv2_connection" "my-connection" {
-#   location = "us-central1"
-#   name = "my-connection"
-#
-#   github_config {
-#     app_installation_id = 123123
-#     authorizer_credential {
-#       oauth_token_secret_version = "projects/my-project/secrets/github-pat-secret/versions/latest"
-#     }
-#   }
-# }
-
-# resource "google_cloudbuildv2_repository" "my-repository" {
-#   name = "K8s"
-#   parent_connection = google_cloudbuildv2_connection.my-connection.id
-#   remote_uri = "https://github.com/Siva-2707/K8s"
-# }
-
-# Step 3: Cloud Build Trigger to build Docker image from GitLab repository
-# resource "google_cloudbuild_trigger" "my_cloud_build_trigger"{
-#   name = "cloudbuild-trigger"
-#   description = "Trigger for building Docker image from GitLab"
-#   location = "us-central1"
-#   filename = "cloudbuild.yml"
-#
-#   trigger_template {
-#     branch_name = "main"
-#     repo_name   = "K8s"
-#   }
-#
-#   approval_config {
-#     approval_required = false
-#   }
-#
-#   github {
-#     owner = "Siva-2707"
-#     name  = "K8s"
-#     push {
-#       branch = "main"
-#     }
-#   }
-# }
-
 # Step 4: Create a Persistent Volume for GKE
-resource "google_compute_disk" "gke_pv" {
-  name  = "gke-pv-disk"
-  size  = 100
-  type  = "pd-standard"
-  zone  = "us-central1-a"
-}
-
-# # Step 5: Create a Persistent Volume Claim for GKE
+# resource "google_compute_disk" "gke_pv" {
+#   name  = "gke-pv"
+#   size  = 10
+#   type  = "pd-standard"
+#   zone  = "us-central1-a"
+# }
+#
+# resource "kubernetes_persistent_volume" "k8s_persistent_volume" {
+#   metadata {
+#     name = "gke-pv-disk"
+#   }
+#   spec {
+#     capacity = {
+#       storage = "10Gi"
+#     }
+#     access_modes = ["ReadWriteMany"]
+#     persistent_volume_source {
+#       gce_persistent_disk {
+#         pd_name = google_compute_disk.gke_pv.name
+#       }
+#     }
+#   }
+# }
+#
+# # # Step 5: Create a Persistent Volume Claim for GKE
 # resource "kubernetes_persistent_volume_claim" "gke_pvc" {
 #   metadata {
 #     name = "gke-pvc"
 #   }
 #   spec {
-#     access_modes = ["ReadWriteOnce"]
+#     access_modes = ["ReadWriteMany"]
 #     resources {
 #       requests = {
-#         storage = "100Gi"
+#         storage = "10Gi"
 #       }
 #     }
-#     volume_name = google_compute_disk.gke_pv.name
+#     volume_name = kubernetes_persistent_volume.k8s_persistent_volume.metadata[0].name
 #   }
 # }
 
-# Step 6: Deploy the container in GKE with Persistent Volume
-# resource "kubernetes_deployment" "gke_deployment" {
-#   metadata {
-#     name = "project-a-deployment"
-#     labels = {
-#       app = "project-a"
-#     }
-#   }
-#   spec {
-#     replicas = 2
-#     selector {
-#       match_labels = {
-#         app = "project-a"
-#       }
-#     }
-#     template {
-#       metadata {
-#         labels = {
-#           app = "project-a"
-#         }
-#       }
-#       spec {
-#         container {
-#           name  = "container-1"
-#           image = "us-central1-docker.pkg.dev/k8s-assignement/my-repository/container-1"
-#           volume_mount {
-#             mount_path = "/app/files"
-#             name       = "gke-pv"
-#           }
-#         }
-#         container {
-#           name  = "container-2"
-#           image = "us-central1-docker.pkg.dev/k8s-assignement/my-repository/container-2"
-#           volume_mount {
-#             mount_path = "/app/files"
-#             name       = "gke-pv"
-#           }
-#         }
-#         volume {
-#           name = "gke-pv"
-#           persistent_volume_claim {
-#             claim_name = kubernetes_persistent_volume_claim.gke_pvc.metadata.0.name
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
-# Step 7: Create a Kubernetes Service to expose the deployment
-# resource "kubernetes_service" "gke_service" {
-#   metadata {
-#     name = "project-a-service"
-#   }
-#   spec {
-#     selector = {
-#       app = "project-a"
-#     }
-#     port {
-#       port        = 80
-#       target_port = 8080
-#     }
-#     type = "LoadBalancer"
-#   }
-# }
 
-output "gke_cluster_name" {
-  value = google_container_cluster.gke_cluster.name
-}
-
-output "gke_cluster_endpoint" {
-  value = google_container_cluster.gke_cluster.endpoint
-}
+# output "gke_cluster_name" {
+#   value = google_container_cluster.gke_cluster.name
+# }
+#
+# output "gke_cluster_endpoint" {
+#   value = google_container_cluster.gke_cluster.endpoint
+# }
